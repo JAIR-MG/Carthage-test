6c9b971e471e369a8fb5ac5d37fb61e55ac08d71
JAIR-MG <jair.moreno@globant.com>
2020-03-04 12:03:35 -0600

    Initial commit

---
+++ CarthageTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarthageTest
+//
+//  Created by Jair Moreno Gaspar on 3/4/20.
+//  Copyright © 2020 Jair Moreno Gaspar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
